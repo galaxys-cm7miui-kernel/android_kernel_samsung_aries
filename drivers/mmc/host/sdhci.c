85bffe794c6576e55af635140845e0633af4a619
Pawit Pornkitprasan <p.pawit@gmail.com>
2011-10-10 12:56:01 +0700

    Merge commit '675f7660ffb0e1880011f6b3c4f9ac241491e3cd' into android-samsung-2.6.35
    (Longterm 2.6.34.14)
    
    Conflicts:
    	drivers/cpuidle/governors/menu.c
    	drivers/mmc/host/sdhci.c
    	sound/soc/codecs/wm8994.c
    
    Change-Id: I812c6a8e1d934f2297ac04a409c92d7bb4d95f35

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1368,6 +1368,13 @@ static void sdhci_tasklet_finish(unsigned long param)
 	if(host == NULL)
 		return;
 
+        /*
+         * If this tasklet gets rescheduled while running, it will
+         * be run again afterwards but without any active request.
+         */
+	if (!host->mrq)
+		return;
+
 	spin_lock_irqsave(&host->lock, flags);
 
 	del_timer(&host->timer);
@@ -1382,7 +1389,7 @@ static void sdhci_tasklet_finish(unsigned long param)
 	 * upon error conditions.
 	 */
 	if (!(host->flags & SDHCI_DEVICE_DEAD) &&
-		(mrq->cmd->error ||
+	    ((mrq->cmd && mrq->cmd->error) ||
 		 (mrq->data && (mrq->data->error ||
 		  (mrq->data->stop && mrq->data->stop->error))) ||
 		   (host->quirks & SDHCI_QUIRK_RESET_AFTER_REQUEST))) {
