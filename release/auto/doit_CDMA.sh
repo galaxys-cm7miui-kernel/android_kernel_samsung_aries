a245d88e3af8217c5961de4745426ddc62e63496
Tk-Glitch <Ti3noU@gmail.com>
2011-10-18 14:58:25 +0200

    Glitch kernel V13 base using Experimental branch up to commit 3cfcd6dd28635f2baa72573dff4d50448dfd3e03

--- release/auto/doit_CDMA.sh
+++ release/auto/doit_CDMA.sh
@@ -18,7 +18,6 @@ REL=CM7${TYPE}-Glitch-DEV-$(date +%Y%m%d_%H%M)-OLDMODEM.zip
 
 	rm -r system 2> /dev/null
 	mkdir  -p system/lib/modules || exit 1
-	mkdir  -p system/lib/hw || exit 1
 	mkdir  -p system/etc/init.d || exit 1
 	mkdir  -p system/etc/glitch-config || exit 1
 	echo "active" > system/etc/glitch-config/screenstate_scaling || exit 1
@@ -27,11 +26,25 @@ REL=CM7${TYPE}-Glitch-DEV-$(date +%Y%m%d_%H%M)-OLDMODEM.zip
 	cd ../
 		find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 	cd release
-	cp 90screenstate_scaling system/etc/init.d/ || exit 1
 	cp lights.aries.so system/lib/hw/ || exit 1
 	cp logcat_module system/etc/init.d/ || exit 1
 	mkdir -p system/bin
 	cp bin/* system/bin/
+
+	# optional folders to go into system
+	if [ -d app ]; then
+		cp -r app system || exit 1
+	fi
+	
+	# stuff in vendor (apparently) overrides other stuff
+	if [ -d vendor ]; then
+		cp -r vendor system || exit 1
+	fi
+	
+	if [ -d lib ]; then
+		cp -r lib system || exit 1
+	fi
+	
 	zip -q -r ${REL} system boot.img META-INF script bml_over_mtd bml_over_mtd.sh || exit 1
 	mv ${REL}* ${TYPE}_OLDMODEM || exit 1
 } || exit 1
